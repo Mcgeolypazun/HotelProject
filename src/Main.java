5424d2ae94b20d638409342680eacaac56078f52
An <taennos1017@naver.com>
2023-10-26 16:50:47 +0900

    Taein_update#1

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
 import customer.Customer;
 import customer.CustomerBuilder;
 import hotelroom.RoomDataClass;
+import tae_in_reservation.MainView;
 
 import java.util.*;
 
 import static hotelroom.RoomDataClass.hotelRoomHashMap;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         RoomDataClass roomDataClass = new RoomDataClass();
+         MainView mainView = new MainView();
+         mainView.mainView();
 
         //점유되고 있는지 확인
-        System.out.println("방 상태:"+hotelRoomHashMap.get("single1"));
+       // System.out.println("방 상태:"+hotelRoomHashMap.get("single1"));
 
         //방을 예약할때
-        hotelRoomHashMap.put("single1",true); // 이 값에 true를 입력함
-        System.out.println("방 상태:"+hotelRoomHashMap.get("single1"));
-        hotelRoomHashMap.put("single1",false);
-
+       // hotelRoomHashMap.put("single1",true); // 이 값에 true를 입력함
+      //  System.out.println("방 상태:"+hotelRoomHashMap.get("single1"));
+       // hotelRoomHashMap.put("single1",false);
 //        Customer customer1 = new CustomerBuilder()
 //                .setMoney(100000)
 //                .setName("김철수")
