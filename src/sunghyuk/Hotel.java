12a636a44eee9772eb442dd624f90c37c8a0e7c9
Mcgeolypazun <taratula0504@gmail.com>
2023-10-27 15:13:23 +0900

    feat : Hotel클래스에 고객정보를 저장하는 reservedCustomer추가함
    sunghyuk패키지안 Hotel클래스
     public static ArrayList<Customer> reservedCustomer = new ArrayList<>();

--- src/sunghyuk/Hotel.java
+++ src/sunghyuk/Hotel.java
@@ -1,11 +1,14 @@
 package sunghyuk;
 
+import customer.Customer;
 import interfaces.HotelRoom;
 
 import java.util.*;
 public class Hotel {
     static ArrayList<HotelRoom> hotelRooms;
     private static int hotelAsset = 1000000000;
+
+    public static ArrayList<Customer> reservedCustomer = new ArrayList<>();
 
     public static int getHotelAsset() {
         return hotelAsset;
--- src/sunghyuk/ReservationRoomDateReady.java
+++ src/sunghyuk/ReservationRoomDateReady.java
@@ -1,5 +1,6 @@
 package sunghyuk;
 
+import hyun_gu.PaymentView;
 import interfaces.HotelRoom;
 
 import java.time.LocalDateTime;
@@ -26,6 +27,8 @@ public class ReservationRoomDateReady {
                 System.out.println("예약을 진행합니다.");
                 arrRoomDate.add(new ReservationRoomDate(hotelRoom, startTime, endTime));//넣으면 arrRoomDate사이즈 증가 한번더 진행
                 //if문을 끝까지 돌면 add함
+                //dong_min패키지의
+                //new PaymentView();
                 break;
             }
             else {
@@ -37,6 +40,7 @@ public class ReservationRoomDateReady {
         if(arrRoomDate.size() == 0){
             System.out.println("예약을 진행합니다.");
             arrRoomDate.add(new ReservationRoomDate(hotelRoom,startTime,endTime));
+            //new PaymentView();
         }
 
     }
